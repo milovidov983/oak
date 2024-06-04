7276f4c940d6f30b8a9bdc23b5a2d24b4234e6e9
milovidov983 <milovidov.vasya@gmail.com>
2024-06-04 21:19:48 +0300

    stage 0

--- cmd/main.go
+++ cmd/main.go
@@ -3,80 +3,36 @@ package main
 import (
 	"fmt"
 	"os"
-
-	"github.com/milovidov983/oak/pkg/models"
 )
 
-type UserInput struct {
-	IsResource   bool
-	IsValid      bool
-	ResourceName string
+type Command struct {
+	Name        string
+	Description string
+	Action      func([]string)
 }
-type Config struct {
-	IsResource bool
+type Result struct {
+	Success bool
+	Message string
 }
 
 func main() {
-	userInput := createUserInput(os.Args)
-	if !userInput.IsValid {
-		fmt.Println("Invalid input")
-		return
-	}
-
-	config, err := createConfig()
+	command, err := parseCommand(os.Args)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	output, err := createOutput(userInput, config)
+	fmt.Println(command)
+	result, err := executeCommand(command)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-
-	if userInput.IsResource {
-		resource, err := findResource(userInput.ResourceName, config)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		password, err := requestPassword()
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		secret.err := resource.Decode(password)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		output.Send(secret)
-
-	}
-
 }
 
-func createOutput(userInput UserInput, config Config) (models.Output, error) {
+func executeCommand(command Command) {
 	panic("unimplemented")
 }
 
-func findResource(s string, config Config) (models.Resource, error) {
-	panic("unimplemented")
-}
-
-func createResource(s string, config Config) {
-	panic("unimplemented")
-}
-
-func createUserInput(s []string) UserInput {
-	// validate user input
-	return UserInput{
-		IsResource: true,
-		IsValid:    true,
-	}
-}
-
-func createConfig() (Config, error) {
-	panic("unimplemented")
+func parseCommand(s []string) Command {
+	return Command{Name: "test", Description: "test", Action: test}
 }
